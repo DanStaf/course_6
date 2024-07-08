afbb5670dd910bbdf3faba0acee519dc0b5adcd3
DanStaf <danstaf@mail.ru>
2024-07-08 12:07:54 +0300

    .env settings added

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
-from secret_data.secret_data import EMAIL_, PASSWORD_
 from pathlib import Path
-import os.path
+import os
+from dotenv import load_dotenv
 
 """
 Django settings for config project.
@@ -19,12 +19,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dot_env = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path=dot_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--6dm&iwt9@u=^*733oe2wo9$c+iykyu8^kyppw4m_*$7c)0j0+'
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,11 +87,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'catalog',  # Название БД
-        'USER': 'postgres',  # Пользователь для подключения
-        'PASSWORD': 'admin',  # Пароль для этого пользователя
-        'HOST': 'localhost',  # Адрес, на котором развернут сервер БД
-        'PORT': 5432,  # Порт, на котором работает сервер БД
+        'NAME': os.getenv('DATABASE_NAME'),  # Название БД
+        'USER': os.getenv('DATABASE_USER'),  # Пользователь для подключения
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),  # Пароль для этого пользователя
+        'HOST': os.getenv('DATABASE_HOST'),  # Адрес, на котором развернут сервер БД
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
@@ -144,8 +147,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 2525
-EMAIL_HOST_USER = EMAIL_
-EMAIL_HOST_PASSWORD = PASSWORD_
+EMAIL_HOST_USER = os.getenv('EMAIL_')
+EMAIL_HOST_PASSWORD = os.getenv('PASSWORD_')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
