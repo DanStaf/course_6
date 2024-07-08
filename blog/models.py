052f59c6ba5d3b80cb195652f915ada32f27002e
DanStaf <danstaf@mail.ru>
2024-07-08 15:39:51 +0300

    Blog created

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     slug = models.CharField(null=True, blank=True, max_length=150, verbose_name='slug')
     content = models.TextField(verbose_name='Содержимое')
-    photo = models.ImageField(null=True, verbose_name='Изображение (превью)')
+    photo = models.ImageField(null=True, blank = True, verbose_name='Изображение (превью)')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
     views_qty = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import main_view
+from django.views.decorators.cache import never_cache
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', main_view, name='home'),
+    path('', ArticleListView.as_view(), name='blog'),
+    path('<int:pk>', ArticleDetailView.as_view(), name='view_article'),
+    path('create', ArticleCreateView.as_view(), name='create_article'), # never_cache
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update_article'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete_article'),
 ]
