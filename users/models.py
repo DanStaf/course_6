9c2435773e35644200b3df1f35d30cb6d9ca3a38
DanStaf <danstaf@mail.ru>
2024-07-08 14:39:07 +0300

    user model added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(verbose_name='?????', unique=True)
+    avatar = models.ImageField(null=True, blank=True, verbose_name='??????')
+    phone = models.CharField(null=True, blank=True, max_length=150, verbose_name='???????')
+    country = models.CharField(null=True, blank=True, max_length=150, verbose_name='??????')
+
+    token = models.CharField(null=True, blank=True, max_length=150, verbose_name='Token')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+
+        if self.first_name and self.last_name:
+            return f'USER: {self.first_name} {self.last_name}'
+        else:
+            return f'USER: {self.email}'
